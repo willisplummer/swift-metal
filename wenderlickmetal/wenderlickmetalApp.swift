ec9b34fd17bee96044740ada578a1e6b22325a38
Willis Plummer <willisplummer@gmail.com>
2023-08-05 00:43:34 -0600

    Initial Commit

---
+++ wenderlickmetal/wenderlickmetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  wenderlickmetalApp.swift
+//  wenderlickmetal
+//
+//  Created by Willis Plummer on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct wenderlickmetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
